f2d070297d72b48b8f8d927af82c7952c6cf5811
Shabbir khan <33508921+shabbirkhan0015@users.noreply.github.com>
2024-10-09 12:37:09 +0530

    first commit

---
+++ src/test/java/com/hubService/HubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
